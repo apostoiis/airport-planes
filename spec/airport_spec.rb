6de1647794e409960e081795d6cd77138e48c280
apostolistax <a.taxidaridis@gmail.com>
2014-04-21 23:10:00 +0100

    Refactored airport_spec also fixed indentation

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,68 +3,61 @@ require 'plane'
 require 'weather'
 
 describe Airport do
-   let(:airport) { Airport.new }
-   let(:plane)   { Plane.new   }
+  let(:airport) { Airport.new }
+  let(:plane)   { Plane.new   }
 
-   it 'is created without any planes' do
-      expect(airport).not_to have_planes
-   end
+  before(:each) { airport.stub(:is_stormy?).and_return(false) }
 
-   it 'can park a plane' do
-      airport.stub(:is_stormy?).and_return(false)
-      airport.land!(plane)
-      expect(airport).to have_planes
-   end
+  it 'is created without any planes' do
+    expect(airport).not_to have_planes
+  end
 
-   it 'is built with a fixed capacity' do
-      airport.stub(:is_stormy?).and_return(false)
-      expect {8.times {airport.land!(plane)}}.to raise_error
-   end
+  it 'can park a plane' do
+    airport.land!(plane)
+    expect(airport).to have_planes
+  end
 
-   it 'knows how many planes are in the aiport' do
-      airport.stub(:is_stormy?).and_return(false)
-      4.times {airport.land!(plane)}
-      expect(airport.plane_count).to eq(4)
-   end
+  it 'is built with a fixed capacity' do
+    expect {8.times {airport.land!(plane)}}.to raise_error
+  end
 
-   it 'a plane can take off' do
-      airport.stub(:is_stormy?).and_return(false)
-      airport.land!(plane)
-      airport.take_off(plane)
-      expect(airport.plane_count).to eq(0)
-   end
+  it 'knows how many planes are in the aiport' do
+    4.times {airport.land!(plane)}
+    expect(airport.plane_count).to eq(4)
+  end
 
-   it 'if it parks a plane it is no longer flying' do
-      airport.stub(:is_stormy?).and_return(false)
-      plane.take_off
-      airport.land!(plane)
-      expect(plane.flying?).to be_false
-   end
+  it 'a plane can take off' do
+    airport.land!(plane)
+    airport.take_off(plane)
+    expect(airport.plane_count).to eq(0)
+  end
 
-   it 'if it takes off a plane it is flying' do
-      airport.stub(:is_stormy?).and_return(false)
-      airport.take_off(plane)
-      expect(plane.flying?).to be_true
-   end
+  it 'if it parks a plane it is no longer flying' do
+    plane.take_off
+    airport.land!(plane)
+    expect(plane.flying?).to be_false
+  end
 
-   it 'if the weather is stormy, a plane cant take off' do
-      airport.stub(:is_stormy?).and_return(false)
-      airport.land!(plane)
-      airport.stub(:is_stormy?).and_return(true)
-      airport.take_off(plane)
-      expect(airport.plane_count).to eq(1)
-      # expect(airport.take_off(plane)).to return "You cant take off the plane with this weather"
-   end
+  it 'if it takes off a plane it is flying' do
+    airport.take_off(plane)
+    expect(plane.flying?).to be_true
+  end
 
-   it 'if the weather is stormy, a plane cant land!' do
-      airport.stub(:is_stormy?).and_return(true)
-      airport.take_off(plane)
-      airport.stub(:is_stormy?).and_return(true)
-      airport.land!(plane)
-      expect(airport.plane_count).to eq 0
-   end
+  it 'if the weather is stormy, a plane cant take off' do
+    airport.land!(plane)
+    airport.stub(:is_stormy?).and_return(true)
+    airport.take_off(plane)
+    expect(airport.plane_count).to eq(1)
+  end
 
-   it '6 planes must land! and then take off allowing for weather' do
-     (1..6).each_with_index {|num,index| Plane.new(index)}
-   end
+  it 'if the weather is stormy, a plane cant land!' do
+    airport.take_off(plane)
+    airport.stub(:is_stormy?).and_return(true)
+    airport.land!(plane)
+    expect(airport.plane_count).to eq 0
+  end
+
+  it '6 planes must land! and then take off allowing for weather' do
+    (1..6).each_with_index {|num,index| Plane.new(index)}
+  end
 end
