dcde0c28b4b9f92399385df50919538b06f68603
apostolistax <a.taxidaridis@gmail.com>
2014-04-21 13:16:17 +0100

    Added lib and spec folders

---
+++ spec/plane_spec.rb
@@ -0,0 +1,10 @@
+require './lib/plane'
+
+# We have planes and airport(s), planes can land and take off and the aiport has a fixed capacity beyond which no more planes can land. On initialization, planes are not flying, each plane has a name. Weather is random, either sunny or stormy, planes can neither land nor take off when weather is stormy
+
+
+# Nice to have: Planes should be created in a hangar (not flying) and transporter can take them to the airport.
+
+describe Plane do
+end
+
